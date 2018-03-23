a94b34cac39db88f2f8decd4e340c5a737db9b8e
shreyanshs7 <shreyanshss7@gmail.com>
2018-03-23 12:18:06 +0530

    updated urls

--- LoginRegister/urls.py
+++ LoginRegister/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import register ,index,registration
+from .views import register ,index,registration,companyList
 
 urlpatterns = [
     url(r'^$', index , name="index"),
     url(r'^registration/',registration),
     url(r'^register/', register , name="register"),
+    url(r'^company/',companyList),
+
 ]
